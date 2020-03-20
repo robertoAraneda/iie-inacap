724c1ae45f2d875322d3d9140ec0d78303c41bd1
claudia_alarcon <c.alarconlazo@gmail.com>
2020-03-20 00:46:00 -0300

    creación modelo, migración y controlador InscritoActividad

---
+++ backend/app/Http/Controllers/InscritoActividadController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\InscritoActividad;
+use App\ReplaceChar;
+use Illuminate\Http\Request;
+
+class InscritoActividadController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $registeredActivities = InscritoActividad::orderBy('idinscritoactividad')->get();
+
+    $registeredActivities = ReplaceChar::replaceStrangeCharacter($registeredActivities);
+
+    return response()->json(['data' => $registeredActivities], 200);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    //
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    $registeredActivity = InscritoActividad::find($id)->get();
+
+    return response()->json(['data' => $registeredActivity], 200);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
+}
---
+++ backend/app/InscritoActividad.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class InscritoActividad extends Model
+{
+  protected $table = 'inscritoactividad';
+}
