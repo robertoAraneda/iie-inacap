934200050ac64ca4b9febb3dd1edc24de4f8191d
roberto <robaraneda@gmail.com>
2020-03-20 19:10:11 -0300

    se corrige nombre de clase palatorma migrate

--- backend/database/migrations/2020_03_20_153719_create_plataforma_table.php
+++ backend/database/migrations/2020_03_20_153719_create_plataforma_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePlataformasTable extends Migration
+class CreatePlataformaTable extends Migration
 {
   /**
    * Run the migrations.
