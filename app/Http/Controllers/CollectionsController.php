012030441a275042c50501c05f9b40d2de9ad6cb
Roberto Araneda Espinoza <robaraneda@gmail.com>
2020-10-19 00:11:31 -0300

    meojra

--- app/Http/Controllers/CollectionsController.php
+++ app/Http/Controllers/CollectionsController.php
@@ -527,6 +527,6 @@ class CollectionsController extends Controller
     foreach ($users as $value) {
       $final[]  = Inscritos::where('idinscrito', $value)->first();
     }
-    return $users;
+    return $final;
   }
 }
